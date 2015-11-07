49dfecddb6732d1629bcc4464025557ff3dca8a8
John Lövrot <john.lovrot@ki.se>
2015-11-07 14:18:41 +0100

    Change back license to CC BY 4.0

--- munge/01-A-munge-varnamconv.R
+++ munge/01-A-munge-varnamconv.R
@@ -1,6 +1,7 @@
 ## Author(s):
 ## John Lövrot (orcid.org/0000-0002-9339-8059)
 ##
-## Licence: MIT
+## Licence: CC BY 4.0
+## https://creativecommons.org/licenses/by/4.0/
 
 rownames(varnamconv) <- varnamconv$variableName
--- src/cache-objects.R
+++ src/cache-objects.R
@@ -1,7 +1,8 @@
 ## Author(s):
 ## John Lövrot (orcid.org/0000-0002-9339-8059)
 ##
-## Licence: MIT
+## Licence: CC BY 4.0
+## https://creativecommons.org/licenses/by/4.0/
 
 unlink(list.files(path="cache", pattern="*", full.names=TRUE))  # clear cache
 ProjectTemplate::reload.project(list(data_loading=TRUE, munging=TRUE))
