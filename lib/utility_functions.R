9ddf0589609720ecb2e7fb44727d71488a5cbb3c
John Lövrot <john.lovrot@ki.se>
2016-03-31 11:07:58 +0200

    Change coding style to http://adv-r.had.co.nz/Style.html

--- lib/utility_functions.R
+++ lib/utility_functions.R
@@ -5,17 +5,17 @@ dist_cor <- function(x, method = "spearman") as.dist(1 - cor(t(x), method = meth
 hclust_avl <- function(x) hclust(x, method = "average")
 
 cat2 <- function(x) {
-    factor(x > median(x),
-        levels = c(FALSE, TRUE),
-        labels = c("Low", "High"))
+  factor(x > median(x),
+    levels = c(FALSE, TRUE),
+    labels = c("Low", "High"))
 }
 
 cat3 <- function(x) {
-    factor(cut(x, c(-Inf, quantile(x, c(1/3, 2/3)), Inf)),
-        c("Low", "Medium", "High"))
+  factor(cut(x, c(-Inf, quantile(x, c(1/3, 2/3)), Inf)),
+    c("Low", "Medium", "High"))
 }
 
 cat4 <- function(x) {
-    factor(cut(x, c(-Inf, quantile(x, c(0.25, 0.5, 0.75)), Inf)),
-        labels = c("Q1", "Q2", "Q3", "Q4"))
+  factor(cut(x, c(-Inf, quantile(x, c(0.25, 0.5, 0.75)), Inf)),
+    labels = c("Q1", "Q2", "Q3", "Q4"))
 }
--- munge/02-C-derive-NPI.R
+++ munge/02-C-derive-NPI.R
@@ -1,7 +1,7 @@
 # https://en.wikipedia.org/wiki/Nottingham_Prognostic_Index
 
 if (all(mainz$tumsizeu == "cm") & all(mainz$lnstat == "LN-"))
-    mainz$NPI <- with(pData(mainz), 0.2*tumsize + 0 + histgrn)
+  mainz$NPI <- with(pData(mainz), 0.2*tumsize + 0 + histgrn)
 
 varMetadata(mainz)["NPI", "labelDescription"] <- "Nottingham prognostic index"
 
