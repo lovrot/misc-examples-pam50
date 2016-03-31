8c9d456eeb41c0d19047ba1ae5fd4011f6063ba2
John Lövrot <john.lovrot@ki.se>
2016-03-31 10:43:58 +0200

    Change coding style to http://adv-r.had.co.nz/Style.html

--- munge/03-A-perform-PAM50-subtype-classification-and-derive-RORS.R
+++ munge/03-A-perform-PAM50-subtype-classification-and-derive-RORS.R
@@ -15,10 +15,10 @@
 ## https://creativecommons.org/licenses/by/4.0/
 
 ## Assemble PAM50centroids object
-data("pam50", package="genefu")
+data("pam50", package = "genefu")
 PAM50centroids <- pam50$centroids
 rownames(PAM50centroids) <- pam50$centroids.map$EntrezGene.ID
-lkup <- c("Basal"="BL", "Her2"="H2", "LumA"="LA", "LumB"="LB", "Normal"="NBL")
+lkup <- c("Basal" = "BL", "Her2" = "H2", "LumA" = "LA", "LumB" = "LB", "Normal" = "NBL")
 colnames(PAM50centroids) <- lkup[colnames(pam50$centroids)]
 
 ## Extract eset with probes annotated with the PAM50 genes
@@ -35,7 +35,7 @@ annotation(eset) <- "org.Hs.eg"
 exprs(eset) <- sweep(exprs(eset), 1, apply(exprs(eset), 1, "median"))
 
 ## Derive centroid correlations
-correlations <- cor(exprs(eset), PAM50centroids[featureNames(eset), ], method="spearman")
+correlations <- cor(exprs(eset), PAM50centroids[featureNames(eset), ], method = "spearman")
 pData(eset) <- cbind(pData(eset), correlations[sampleNames(eset), ])
 
 ## Classify to nearest centroid
@@ -43,10 +43,10 @@ pData(eset)$subtypecd <- colnames(PAM50centroids)[apply(correlations[sampleNames
 
 ## Derive ROR-S score  (Risk of reccurence score, subtype alone)
 ## Coefficients from
-## Parker09JCO:p1162, ROR-S = 0.05*basal + 0.12*Her2 - 0.34*LumA + 0.23*LumB
-## Nielsen10CCR:Suppl ROR-S = 0.05*Basal + 0.12*Her2 - 0.34*LumA + 0.23*LumB
+## Parker09JCO:p1162, ROR-S  =  0.05*basal + 0.12*Her2 - 0.34*LumA + 0.23*LumB
+## Nielsen10CCR:Suppl ROR-S  =  0.05*Basal + 0.12*Her2 - 0.34*LumA + 0.23*LumB
 pData(eset) <- within(pData(eset), {
-    RORS <- 0.05*BL + 0.12*H2 + -0.34*LA + 0.23*LB
+  RORS <- 0.05*BL + 0.12*H2 + -0.34*LA + 0.23*LB
 })
 
 ## Append to ExpressionSet
@@ -54,16 +54,16 @@ mainz$subtypecd <- pData(eset)[sampleNames(mainz), "subtypecd"]
 mainz$RORS <- pData(eset)[sampleNames(mainz), "RORS"]
 
 lkup <- c(
-    "LA"="Luminal A",
-    "LB"="Luminal B",
-    "H2"="HER2-enriched",
-    "BL"="Basal-like",
-    "NBL"="Normal breast-like")
+  "LA" = "Luminal A",
+  "LB" = "Luminal B",
+  "H2" = "HER2-enriched",
+  "BL" = "Basal-like",
+  "NBL" = "Normal breast-like")
 mainz$subtype <- lkup[as.character(mainz$subtypecd)]
 
 ## Turn into factors
-mainz$subtypecd <- factor(mainz$subtypecd, levels=names(lkup))
-mainz$subtype <- factor(mainz$subtype, levels=lkup)
+mainz$subtypecd <- factor(mainz$subtypecd, levels = names(lkup))
+mainz$subtype <- factor(mainz$subtype, levels = lkup)
 
 pick <- c("subtypecd", "subtype")
 varMetadata(mainz)[pick, "labelDescription"] <- varnamconv[pick, "labelDescription"]
